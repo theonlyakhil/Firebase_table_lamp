ff5c4bb10a470360d2ddd2a819d0482f0c3d9a1f
Akhil A B <akhilnanosoft79@gmail.com>
2019-10-08 21:37:14 +0530

    Add files via upload

---
+++ firebase_table_lamp.ino
@@ -0,0 +1,58 @@
+//
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
+
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "tablelamp-c71a6.firebaseio.com"
+#define FIREBASE_AUTH "iC9JQ2rXi66rSh8sC77bp7KPc1FLX79uFPf8r4RC"
+#define WIFI_SSID "Google pixel3"
+#define WIFI_PASSWORD "12345678"
+
+void setup() {
+  Serial.begin(9600);
+
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+int n = 0;
+
+void loop() {
+ 
+
+  // get value 
+  
+  Serial.println(Firebase.getString("data"));
+  
+  delay(1000);
+
+  
+}
